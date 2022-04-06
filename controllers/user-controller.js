228b54030145fa9dced9a01febca66e06037d8f7
ckyle121 <carolinekyle21@gmail.com>
2022-04-06 11:06:45 -0500

    api routes for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -51,6 +51,23 @@ const userController = {
             });
     },
 
+    // update a user by id
+    updateUser({ params }, res){
+        User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                // if no user found, send 404
+                if (!dbUserData){
+                    res.status(404).json({ message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
     // delete a user
     deleteUser({ params }, res){
         User.findOneAndDelete({ _id: params.id })
---
+++ routes/api/user-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+// api/users/ <GET, POST> routes
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:id <GET, PUT, DELETE> routes
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// /api/users/:userId/friends/:friendId <POST, DELETE> 
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+module.exports - router; 
